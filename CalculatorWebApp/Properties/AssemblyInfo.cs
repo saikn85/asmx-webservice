217a4169acd4b847ba81dd9ed135170ed8b55a97
saikn85 <85madmaxfury@gmail.com>
2023-03-08 20:59:23 +0530

    day 2

---
+++ CalculatorWebApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CalculatorWebApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorWebApp")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a3d22d4e-28f5-43ce-9ad9-f2778c1bde4a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalculatorWebApp/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CalculatorWebApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Enter First Number</td>
+                    <td>
+                        <asp:TextBox ID="txtfirstNum" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Enter Second Number</td>
+                    <td>
+                        <asp:TextBox ID="txtSecondNum" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Result</td>
+                    <td><asp:Label ID="lblResult" runat="server"></asp:Label></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Button ID="btnAdd" runat="server" Text="Sum" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
